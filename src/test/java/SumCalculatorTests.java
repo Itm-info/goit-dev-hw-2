1e99879374ea0b809ee4076e6ab383299dd5ab0a
Itm-info <itm@infotech.net.ua>
2023-06-11 19:16:15 +0300

    Initial commit

---
+++ src/test/java/SumCalculatorTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SumCalculatorTest {
+
+    @BeforeEach
+    public void beforeEach() {
+        calc = new SumCalculator();
+    }
+    @Test
+    public void testSum(){
+        Assertions.assertArrayEquals(10, new );
+    }
+
+}

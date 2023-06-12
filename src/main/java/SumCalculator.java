cdc0bbb89afe2aac4fb3677719959438c5a3ef21
Itm-info <itm@infotech.net.ua>
2023-06-12 12:39:35 +0300

    Oops forgot to push

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SumCalculator {
+import static java.lang.Math.abs;
+
+public class SumCalculator {
+    static int sum(int n) {
+        if (n == 0) throw new IllegalArgumentException("Must not be zero");
+
+        int step = n/abs(n);
+        int res = 0;
+        int i;
+        for (i=1;(i/n) != 1;i += step) {
+            res += i;
+        }
+        return res + i;
+    }
+
+    public static void main(String[] args) {
+    }
 }
--- src/test/java/SumCalculatorTests.java
+++ src/test/java/SumCalculatorTests.java
@@ -2,15 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SumCalculatorTest {
+class SumCalculatorTests {
+    private SumCalculator calc;
 
     @BeforeEach
     public void beforeEach() {
         calc = new SumCalculator();
     }
-    @Test
-    public void testSum(){
-        Assertions.assertArrayEquals(10, new );
-    }
 
+    @Test
+    public void testSumCalculator_3(){
+        Assertions.assertEquals(-5, calc.sum(-3) );
+    }
+    @Test
+    public void testSumCalculator1(){
+        Assertions.assertEquals(1, calc.sum(1) );
+    }
+    @Test
+    public void testSumCalculator4(){
+        Assertions.assertEquals(10, calc.sum(4) );
+    }
+    @Test
+    public void testSumCalculator0(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calc.sum(0));
+    }
 }
